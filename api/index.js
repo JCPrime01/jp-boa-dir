2e4967aea4f5a16ae42bdef7ede9176b83ad979a
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-17 10:00:50 -0300

    chore: update shareCode to L57YFN3U78T

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6S9169Y63PJ&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=L57YFN3U78T&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
