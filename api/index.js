0b73b900f08bc8aca9fd810ec43830b16c1e9ba1
JCPrime01 <vanzokiller@gmail.com>
2026-07-02 16:52:07 -0300

    chore: shareCode 1PV46V5KCDI -> 2R9ND4VG2ZQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=1PV46V5KCDI&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2R9ND4VG2ZQ&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
