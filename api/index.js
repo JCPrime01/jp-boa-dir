74da4855408d6110e172cad5a71cb3382435dcb9
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 08:41:01 -0300

    chore: shareCode E7CEVXA00KX -> T1IHC8HMEWW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=E7CEVXA00KX&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=T1IHC8HMEWW&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
