a047db2d4e3e31a2cba732e128bdc24d4408bba8
JCPrime01 <vanzokiller@gmail.com>
2026-07-14 11:31:43 -0300

    chore: shareCode 7GHUB9AT6L3 -> 24CF6NBT74A (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=7GHUB9AT6L3&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=24CF6NBT74A&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
