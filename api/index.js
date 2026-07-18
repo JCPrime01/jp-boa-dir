4f69504b675da43cd6f7652ee0413f01aea79bae
JCPrime01 <vanzokiller@gmail.com>
2026-07-18 12:57:03 -0300

    chore: shareCode BMTMGRB9ZN0 -> CR5AYPYVGF4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=BMTMGRB9ZN0&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=CR5AYPYVGF4&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
