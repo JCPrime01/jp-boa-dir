cde8e14d647648f80455720e086ee30ca362b2ed
JCPrime01 <vanzokiller@gmail.com>
2026-07-21 11:46:25 -0300

    chore: shareCode UHVAAROR19J -> 33FELV5BH6W (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=UHVAAROR19J&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=33FELV5BH6W&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
