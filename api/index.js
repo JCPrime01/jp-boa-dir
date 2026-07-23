17e23b29059f393b86ae19974cfc65a1d17b12bf
JCPrime01 <vanzokiller@gmail.com>
2026-07-23 11:41:31 -0300

    chore: shareCode 1W1Z81S4WEQ -> MG1NO2K5DU6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=1W1Z81S4WEQ&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=MG1NO2K5DU6&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
