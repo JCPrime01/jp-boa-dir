970674b6c2b1e8921159b9190d1e4d9b83b770a3
JCPrime01 <vanzokiller@gmail.com>
2026-08-02 11:41:26 -0300

    chore: shareCode WO4S52O4PUI -> F6AFROI4DGV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=WO4S52O4PUI&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=F6AFROI4DGV&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
