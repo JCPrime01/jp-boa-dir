6aec6443c9ff4ba885e711031cfc39c80ce95076
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 15:18:35 -0300

    chore: shareCode VOA6GTMNQ81 -> TAFAFMPVSAD (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=VOA6GTMNQ81&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=TAFAFMPVSAD&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
