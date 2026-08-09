d0e5780109fdf8b9a10ae3fa4ace3415834b770c
JCPrime01 <vanzokiller@gmail.com>
2026-08-09 10:54:12 -0300

    chore: shareCode TAFAFMPVSAD -> YBAAYA1GOBQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=TAFAFMPVSAD&afp2=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=YBAAYA1GOBQ&afp2=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
